6f03fbe4b82b811d271c89a0a95febe601f4cf56
ikeda <ikeda@jokerpiece.co.jp>
2017-06-06 19:33:26 +0900

    gulpテンプレート追記

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require("gulp-autoprefixer");
 var uglify = require("gulp-uglify");
 var plumber = require("gulp-plumber");
 var minify = require("gulp-minify-css");
+var imgmin = require('gulp-imagemin');
 
 gulp.task("sass",function(){
 	gulp.src("scss/style.scss")
@@ -22,8 +23,16 @@ gulp.task("js",function(){
 	gulp.src(["js/**/*.js","!js/min/**/*.js"])
 	.pipe(plumber())
 	.pipe(uglify())
-	.pipe(gulp.dest("./js/min"));
+	.pipe(gulp.dest("js/min"));
 })
+
+// 画像圧縮
+gulp.task('img',function() {
+	var imgpath = 'imgsrc/*.+(png|jpeg|jpg|svg)';
+	gulp.src(imgpath)
+	.pipe(imgmin())
+	.pipe(gulp.dest('img/'));
+}); 
 
 gulp.task("default",function(){
 	gulp.watch("scss/**/*scss",["sass"]);
