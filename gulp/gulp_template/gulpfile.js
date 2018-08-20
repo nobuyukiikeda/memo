466a2ab550501075dd054f0bdeb3095147326d9d
nobuyuki <nnipsii@gmial.com>
2018-08-20 10:26:03 +0900

    gulpテンプレート修正

--- gulp/gulp_template/gulpfile.js
+++ gulp/gulp_template/gulpfile.js
@@ -1,28 +1,32 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var cleanCss = require('gulp-clean-css');
-var imgmin = require('gulp-imagemin');
-var pug = require('gulp-pug');
-var sourcemaps = require('gulp-sourcemaps');
-var notify = require('gulp-notify');
-var rename = require('gulp-rename');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+const gulp = require('gulp'),
+	  sass = require('gulp-sass'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  plumber = require('gulp-plumber'),
+	  uglify = require('gulp-uglify'),
+	  cleanCss = require('gulp-clean-css'),
+	  imagemin = require('gulp-imagemin'),
+	  pngquant = require('imagemin-pngquant'),
+	  mozjpeg = require('imagemin-mozjpeg'),
+	  pug = require('gulp-pug'),
+	  sourcemaps = require('gulp-sourcemaps'),
+	  notify = require('gulp-notify');
 
-gulp.task('pug',function(){
+gulp.task('log', done => {
+	console.log('gulp done!')
+	done()
+})
+
+gulp.task('pug', done => {
 	gulp.src(['./pug/*.pug', '!./pug/_*.pug'])
 	.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
 	.pipe(pug({
 		pretty: true
 	}))
-	.pipe(gulp.dest('./html/'));
+	.pipe(gulp.dest('./html/'))
+	done()
 });
 
-gulp.task('sass',function(){
+gulp.task('sass', done => {
 	gulp.src('./scss/style.scss')
 	.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
 	.pipe(sourcemaps.init())
@@ -30,42 +34,45 @@ gulp.task('sass',function(){
 	.pipe(autoprefixer({
 		browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4']
 	}))
-	// .pipe(cleanCss())
+	.pipe(cleanCss())
 	.pipe(sourcemaps.write('./maps'))
 	.pipe(gulp.dest('./css/'))
+	done()
 });
 
-gulp.task('watch',function(){
-	gulp.watch('./scss/**/*.scss',['sass']);
-});
-
-gulp.task('js',function(){
+gulp.task('js', done => {
 	gulp.src(['./js/src/*.js', './js/src/**/*.js'])
 	.pipe(plumber())
-	// .pipe(uglify())
-	.pipe(gulp.dest('./js/min/'));
+	.pipe(uglify())
+	.pipe(gulp.dest('./js/min/'))
+	done()
 })
 
-// Babel
-gulp.task('babel', function(){
-	browserify('./js/src/app.js', { debug: true })
-	.transform(babelify, {presets: ['es2015']})
-	.bundle()
-	.on('error', function(err){ console.log('Error : ' + err.message); })
-	.pipe(source('app.js'))
-	.pipe(gulp.dest('js/dist'))
-});
 
 // 画像圧縮
-gulp.task('img',function() {
-	gulp.src('./img_src/**/**')
-	.pipe(imgmin())
-	.pipe(gulp.dest('./img/'));
+gulp.task('img', done =>  {
+	gulp.src(['./_img/**/**', './_img/**'])
+	.pipe(imagemin([
+		pngquant({
+			quality: '65-80',  // 画質
+			speed: 1,  // 最低のスピード
+			floyd: 0,  // ディザリングなし
+		}),
+		mozjpeg({
+			quality: 85, // 画質
+			progressive: false
+		}),
+		imagemin.svgo(),
+		imagemin.optipng(),
+		imagemin.gifsicle()
+	]))
+	.pipe(gulp.dest('./img/'))
+	done()
 }); 
 
-gulp.task('default',function(){
-	gulp.watch('./pug/**/*.pug',['pug']);
-	gulp.watch('./scss/**/*.scss',['sass']);
-	// gulp.watch('./img_src/**/**',['img']);
-	// gulp.watch(['./js/**/*js','!./js/min/**/*.js'],['js']);
+gulp.task('default', () => {
+	gulp.watch('./pug/**/*.pug', gulp.series('pug', 'log'));
+	gulp.watch('./scss/**/*.scss', gulp.series('sass', 'log'));
+	gulp.watch('./img_src/**/**', gulp.series('img', 'log'));
+	gulp.watch(['./js/**/*js','!./js/min/**/*.js'], gulp.series('js', 'log'));
 })
